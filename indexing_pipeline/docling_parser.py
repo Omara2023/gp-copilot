d88305c1195e974fe9ce66f6a58cee9335ba2922
Omara2023 <144819892+Omara2023@users.noreply.github.com>
2026-09-22 11:02:43 +0100

    Correct docling parser.

--- indexing_pipeline/docling_parser.py
+++ indexing_pipeline/docling_parser.py
@@ -1,16 +1,23 @@
 from pathlib import Path
 from docling.document_converter import DocumentConverter
+from docling_core.types.doc.document import DoclingDocument
+from docling.chunking import HybridChunker
 from indexing_pipeline.document_parser import DocumentParser
 
 class DoclingParser(DocumentParser):
     """Open source Docling parser."""
 
     def __init__(self) -> None:
-        self.converter = DocumentConverter()
+        self.converter = DocumentConverter() 
+        self.chunker = HybridChunker() #consider DI for both
 
-    def __parse__(self, path: str) -> str:  
+    def _parse(self, path: str) -> DoclingDocument:  
         p = Path(path)
         if p.exists() and p.is_file() and p.name.endswith(".pdf"):  
-            result = self.converter.convert(path)
-            return result.document.export_to_text()
-        return ""
+            return self.converter.convert(path)
+        return DoclingDocument()
+
+    def chunk(self, doc: DoclingDocument):
+        return [chunk for chunk in self.chunker.chunk(doc)]
+        #llm metadata extraction & embedding. this needs to be a langraph parallelisation    
+        #could parallelise this with asyncio or do langrpaph on the collection as a whole, evaluate/consider both.
