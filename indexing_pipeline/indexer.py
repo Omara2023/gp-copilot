1d16f14531b4ea34482cc9fff7839dd6c3d2930f
Omara2023 <144819892+Omara2023@users.noreply.github.com>
2026-09-25 12:01:32 +0100

    Move indexer to pipeline module and remove constants.

---
+++ indexing_pipeline/indexer.py
@@ -0,0 +1,61 @@
+from pydantic import BaseModel
+from langgraph.graph import START, END, StateGraph
+from docling_core.transforms.chunker.base import BaseChunk
+from docling_core.types.doc.document import DoclingDocument
+from models.clinical_metadata import ClinicalMetadata
+from indexing_pipeline.docling_parser import DoclingParser
+from indexing_pipeline.chroma_client import ChromaClient
+from indexing_pipeline.clinical_metadata_extractor import ClinicalMetadataExtractor
+
+class IndexerState(BaseModel):
+    path: str
+    document: DoclingDocument | None = None
+    chunks: list[BaseChunk]
+    metadata: list[ClinicalMetadata] 
+
+class Indexer:
+
+    def __init__(self, parser: DoclingParser, metadata_extractor: ClinicalMetadataExtractor, vector_client: ChromaClient):
+        self.parser = parser
+        self.metadata_extractor = metadata_extractor
+        self.vector_client = vector_client
+
+        self.graph = self._build_graph()
+
+    def _build_graph(self):
+        builder = StateGraph(IndexerState)
+        
+        builder.add_node("document_processing", self.document_processing)
+        builder.add_node("metadata_extraction", self.metadata_extraction)
+        builder.add_node("injection", self.injection)
+    
+        builder.add_edge(START, "document_processing")
+        builder.add_edge("document_processing", "metadata_extraction")
+        builder.add_edge("metadata_extraction", "injection")
+        builder.add_edge("injection", END)
+        
+        return builder.compile()
+
+    def document_processing(self, state: IndexerState) -> dict:
+        document = self.parser.read(state["path"])
+        chunks = self.parser.chunk(document)
+        return {"document": document, "chunks": chunks}
+
+    def metadata_extraction(self, state: IndexerState) -> dict:
+        chunks = state["chunks"]
+        metadata = []
+        for c in chunks:
+            result = self.metadata_extractor.invoke(c)
+            metadata.append(result)
+
+        return {"metadata": metadata}
+
+    def injection(self, state: IndexerState) -> dict:
+        chunks = state["chunks"]
+        metadata = [m.model_dump() for m in state["metadata"]]
+        ids = [chunk.id for chunk in chunks]
+        self.vector_client.add(docuements=chunks, metadatas=metadata, ids=ids)
+        return {}
+
+    def run(self, path: str):
+        self.graph.invoke({"path": path})
